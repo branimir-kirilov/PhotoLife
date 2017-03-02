d1111588f63b145f22e42b836dbeddfe1722d45f
Branimir <brani_svetoslavov@abv.bg>
2017-03-02 14:30:54 +0200

    Add unit of work

--- PhotoLife/PhotoLife.Data/GenericRepository.cs
+++ PhotoLife/PhotoLife.Data/GenericRepository.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 using PhotoLife.Data.Contracts;
 
 namespace PhotoLife.Data
---
+++ PhotoLife/PhotoLife.Data/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.Entity;
+using PhotoLife.Data.Contracts;
+
+namespace PhotoLife.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext dbContext;
+
+        public UnitOfWork(DbContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("Context cannot be null");
+            }
+
+            this.dbContext = context;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public void Commit()
+        {
+            this.dbContext.SaveChanges();
+        }
+    }
+}
