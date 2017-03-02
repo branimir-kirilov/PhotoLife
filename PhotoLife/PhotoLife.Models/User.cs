3e8ca4d479151ce34455cab3773426abd7aa5193
Branimir <brani_svetoslavov@abv.bg>
2017-03-02 13:32:05 +0200

    Add user model

---
+++ PhotoLife/PhotoLife.Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace PhotoLife.Models
+{
+    public class User : IdentityUser
+    {
+        public User() : base(string.Empty)
+        {
+            
+        }
+
+        public User(string username, string email, string name, string description)
+        {
+            this.UserName = username;
+            this.Email = email;
+            this.Description = description;
+        }
+
+        public string Description { get; set; }
+
+        public Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager)
+        {
+            return manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+        }
+    }
+}
