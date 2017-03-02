f7ad8c2aaca72e0fb6f619b52475a9f6b6268fe2
Branimir <brani_svetoslavov@abv.bg>
2017-03-02 11:58:37 +0200

    Add project basic structure

---
+++ PhotoLife/PhotoLife/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PhotoLife.Startup))]
+namespace PhotoLife
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
