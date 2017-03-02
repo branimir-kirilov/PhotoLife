2c42338851b45e99f540e8a7d0cf901738c5e28b
Branimir <brani_svetoslavov@abv.bg>
2017-03-02 13:17:34 +0200

    Add less preprocessor

--- PhotoLife/PhotoLife.Web/App_Start/BundleConfig.cs
+++ PhotoLife/PhotoLife.Web/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace PhotoLife
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.less"));   
         }
     }
 }
