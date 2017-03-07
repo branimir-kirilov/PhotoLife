c4b63872ccc1754f592dd22b38f1fd82e67e82c7
Branimir <brani_svetoslavov@abv.bg>
2017-03-07 17:57:03 +0200

    Add entities interface

---
+++ PhotoLife/PhotoLife.Data/Contracts/IPhotoLifeEntities.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+
+namespace PhotoLife.Data.Contracts
+{
+    public interface IPhotoLifeEntities
+    {
+        IDbSet<TEntity> DbSet<TEntity>()
+         where TEntity : class;
+
+        int SaveChanges();
+
+        void SetAdded<TEntry>(TEntry entity)
+            where TEntry : class;
+
+        void SetDeleted<TEntry>(TEntry entity)
+            where TEntry : class;
+
+        void SetUpdated<TEntry>(TEntry entity)
+            where TEntry : class;
+    }
+}
