bc21aeaf79e8674f3e7789a06fcd29a34cf1dc3f
Branimir <brani_svetoslavov@abv.bg>
2017-03-07 15:17:04 +0200

    Moving authentication complete

--- PhotoLife/PhotoLife.Web/App_Start/Startup.Auth.cs
+++ PhotoLife/PhotoLife.Web/App_Start/Startup.Auth.cs
@@ -5,6 +5,7 @@ using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
+using PhotoLife.Authentication.ApplicationManagers;
 using PhotoLife.Data;
 using PhotoLife.Models;
 
--- PhotoLife/PhotoLife.Web/Controllers/AccountController.cs
+++ PhotoLife/PhotoLife.Web/Controllers/AccountController.cs
@@ -8,6 +8,7 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
+using PhotoLife.Authentication.ApplicationManagers;
 using PhotoLife.Models;
 
 namespace PhotoLife.Controllers
