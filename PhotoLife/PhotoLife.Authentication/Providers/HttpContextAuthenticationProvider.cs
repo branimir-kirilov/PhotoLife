bb1298d535e93eba66e8dd622034e2c4ddd57376
Branimir <brani_svetoslavov@abv.bg>
2017-03-07 15:12:48 +0200

    Add authentication provider

---
+++ PhotoLife/PhotoLife.Authentication/Providers/HttpContextAuthenticationProvider.cs
@@ -0,0 +1,64 @@
+﻿using System.Web;
+using PhotoLife.Authentication.ApplicationManagers;
+using PhotoLife.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace PhotoLife.Authentication.Providers
+{
+    public class HttpContextAuthenticationProvider : IAuthenticationProvider
+    {
+        protected ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return HttpContext.Current.GetOwinContext().GetUserManager<ApplicationSignInManager>();
+            }
+        }
+        protected ApplicationUserManager UserManager
+        {
+            get
+            {
+                return HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+        }
+
+        public bool IsAuthenticated
+        {
+            get
+            {
+                return HttpContext.Current.User.Identity.IsAuthenticated;
+            }
+        }
+
+        public string CurrentUserId
+        {
+            get
+            {
+                return HttpContext.Current.User.Identity.GetUserId();
+            }
+        }
+
+        public IdentityResult RegisterAndLoginUser(User user, string password, bool isPersistent, bool rememberBrowser)
+        {
+            var result = this.UserManager.Create(user, password);
+
+            if (result.Succeeded)
+            {
+                this.SignInManager.SignIn(user, isPersistent, rememberBrowser);
+            }
+
+            return result;
+        }
+
+        public SignInStatus SignInWithPassword(string email, string password, bool rememberMe, bool shouldLockout)
+        {
+            return this.SignInManager.PasswordSignIn(email, password, rememberMe, shouldLockout);
+        }
+
+        public void SignOut()
+        {
+            HttpContext.Current.GetOwinContext().Authentication.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+        }
+    }
+}
---
+++ PhotoLife/PhotoLife.Authentication/Providers/IAuthenticationProvider.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using PhotoLife.Models;
+
+namespace PhotoLife.Authentication.Providers
+{
+    public interface IAuthenticationProvider
+    {
+        bool IsAuthenticated { get; }
+
+        string CurrentUserId { get; }
+
+        IdentityResult RegisterAndLoginUser(User user, string password, bool isPersistent, bool rememberBrowser);
+
+        SignInStatus SignInWithPassword(string email, string password, bool rememberMe, bool shouldLockout);
+
+        void SignOut();
+    }
+}
