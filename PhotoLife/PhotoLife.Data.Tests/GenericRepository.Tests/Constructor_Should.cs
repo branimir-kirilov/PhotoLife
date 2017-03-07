8e244a89870d6318e0e37197eaf34ee6d4581cca
Branimir <brani_svetoslavov@abv.bg>
2017-03-07 18:23:27 +0200

    Finished constructor tests

--- PhotoLife/PhotoLife.Data.Tests/GenericRepository.Tests/Constructor_Should.cs
+++ PhotoLife/PhotoLife.Data.Tests/GenericRepository.Tests/Constructor_Should.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Moq;
 using NUnit.Framework;
 using PhotoLife.Data.Tests.GenericRepository.Tests.Mocks;
 using PhotoLife.Data;
@@ -13,6 +14,42 @@ namespace PhotoLife.Data.Tests.GenericRepository.Tests
         {
             //Arrange & Act & Assert
             Assert.Throws<ArgumentNullException>(() => new GenericRepository<MockedGenericRepositoryType>(null));
+        }
+
+        [Test]
+        public void _NotThrow_ArgumentNullException_IfDbContext_NotNull()
+        {
+            //Arrange
+            var mockedDbContext = new Mock<PhotoLifeEntities>();
+            //Act & Assert
+            Assert.DoesNotThrow(() => new GenericRepository<MockedGenericRepositoryType>(mockedDbContext.Object));
+        }
+
+        [Test]
+        public void _Initialize_NotNull_IfDbContext_NotNull()
+        {
+            //Arrange
+            var mockedDbContext = new Mock<PhotoLifeEntities>();
+           
+            //Act
+            var genericRepository = new GenericRepository<MockedGenericRepositoryType>(mockedDbContext.Object);
+
+            //Assert
+            Assert.IsNotNull(genericRepository);
+        }
+
+
+        [Test]
+        public void _Initialize_CorrectInstance_IfDbContext_NotNull()
+        {
+            //Arrange
+            var mockedDbContext = new Mock<PhotoLifeEntities>();
+
+            //Act
+            var genericRepository = new GenericRepository<MockedGenericRepositoryType>(mockedDbContext.Object);
+
+            //Assert
+            Assert.IsInstanceOf<GenericRepository<MockedGenericRepositoryType>>(genericRepository);
         }
     }
 }
