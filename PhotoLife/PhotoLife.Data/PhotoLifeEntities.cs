cd5cf03a397c1010910009d2c6c215f74e78431a
Branimir <brani_svetoslavov@abv.bg>
2017-03-07 17:59:18 +0200

    Implemented PhotoLife entities interface

--- PhotoLife/PhotoLife.Data/PhotoLifeEntities.cs
+++ PhotoLife/PhotoLife.Data/PhotoLifeEntities.cs
@@ -1,11 +1,12 @@
 ﻿using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
 using Microsoft.AspNet.Identity.EntityFramework;
+using PhotoLife.Data.Contracts;
 using PhotoLife.Models;
 
 namespace PhotoLife.Data
 {
-    public class PhotoLifeEntities : IdentityDbContext<User>
+    public class PhotoLifeEntities : IdentityDbContext<User>, IPhotoLifeEntities
     {
         public PhotoLifeEntities()
             : base("PhotoLifeDb", throwIfV1Schema: false)
@@ -36,6 +37,28 @@ namespace PhotoLife.Data
         public DbSet<Category> Categories { get; set; }
 
         public DbSet<Comment> Comments { get; set; }
-     
+
+        public IDbSet<TEntity> DbSet<TEntity>() where TEntity : class
+        {
+            return this.Set<TEntity>();
+        }
+
+        public void SetAdded<TEntry>(TEntry entity) where TEntry : class
+        {
+            var entry = this.Entry(entity);
+            entry.State = EntityState.Added;
+        }
+
+        public void SetDeleted<TEntry>(TEntry entity) where TEntry : class
+        {
+            var entry = this.Entry(entity);
+            entry.State = EntityState.Deleted;
+        }
+
+        public void SetUpdated<TEntry>(TEntry entity) where TEntry : class
+        {
+            var entry = this.Entry(entity);
+            entry.State = EntityState.Modified;
+        }
     }
 }
