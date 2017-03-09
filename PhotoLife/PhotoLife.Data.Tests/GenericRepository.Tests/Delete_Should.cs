4318ecafb3b167f6a4400fc58cb0e8934739bc21
Branimir <brani_svetoslavov@abv.bg>
2017-03-09 13:31:49 +0200

    Add Generic Repository Delete() Tests

---
+++ PhotoLife/PhotoLife.Data.Tests/GenericRepository.Tests/Delete_Should.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using PhotoLife.Data.Contracts;
+using PhotoLife.Data.Tests.GenericRepository.Tests.Mocks;
+
+namespace PhotoLife.Data.Tests.GenericRepository.Tests
+{
+    [TestFixture]
+    public class Delete_Should
+    {
+        [Test]
+        public void _Call_DbContext_SetDeleted_Correctly()
+        {
+            //Arrange
+            var mockedDbContext = new Mock<IPhotoLifeEntities>();
+
+            var repository = new GenericRepository<MockedGenericRepositoryType>(mockedDbContext.Object);
+
+            var entity = new Mock<MockedGenericRepositoryType>();
+
+            //Act
+            repository.Delete(entity.Object);
+
+            //Assert
+            mockedDbContext.Verify(mdb => mdb.SetDeleted(entity.Object), Times.Once);
+        }
+    }
+}
