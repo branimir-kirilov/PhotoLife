9bbca71d26695d363e54e8e33b55eb7c9a344196
Branimir <brani_svetoslavov@abv.bg>
2017-03-09 13:30:06 +0200

    Add Generic Repository Update() Tests

---
+++ PhotoLife/PhotoLife.Data.Tests/GenericRepository.Tests/Update_Should.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using NUnit.Framework;
+using PhotoLife.Data.Contracts;
+using PhotoLife.Data.Tests.GenericRepository.Tests.Mocks;
+
+namespace PhotoLife.Data.Tests.GenericRepository.Tests
+{
+    [TestFixture]
+    public class Update_Should
+    {
+        [Test]
+        public void _Call_SetUpdated_Correctly()
+        {
+            //Arrange
+            var mockedDbContext = new Mock<IPhotoLifeEntities>();
+
+            var repository = new GenericRepository<MockedGenericRepositoryType>(mockedDbContext.Object);
+
+            var entity = new Mock<MockedGenericRepositoryType>();
+
+            //Act
+            repository.Update(entity.Object);
+
+            //Assert
+            mockedDbContext.Verify(mdb => mdb.SetUpdated(entity.Object), Times.Once);
+        }
+    }
+}
