2fa63b8892463198db46e90b330c9063a00844cc
Branimir <brani_svetoslavov@abv.bg>
2017-03-14 16:30:32 +0200

    Add log off tests class

---
+++ PhotoLife/PhotoLife.Web.Tests/Controllers/Account/LogOff_Should.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using PhotoLife.Authentication.Providers;
+using PhotoLife.Controllers;
+using PhotoLife.Factories;
+
+namespace PhotoLife.Web.Tests.Controllers.Account
+{
+    [TestFixture]
+    public class LogOff_Should
+    {
+        [Test]
+        public void _ShouldCall_Provider_LogOout()
+        {
+            //Arrange
+            var mockedProvider = new Mock<IAuthenticationProvider>();
+            var mockedFactory = new Mock<IUserFactory>();
+
+            var controller = new AccountController(mockedProvider.Object, mockedFactory.Object);
+          
+            //Act
+            controller.LogOff();
+
+            //Assert
+            mockedProvider.Verify(p => p.SignOut(), Times.Once);
+        }
+    }
+}
