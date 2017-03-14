3bf90f151a5fc6bb594d2e24204c93b04015fc40
Branimir <brani_svetoslavov@abv.bg>
2017-03-14 20:35:32 +0200

    Add new test assembly

---
+++ PhotoLife/PhotoLife.Services.Tests/UserService/Constructor_Should.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoLife.Services.Tests.UserService
+{
+    [TestFixture]
+    class Constructor_Should
+    {
+    }
+}
