48186c250ca02c8e00353d3be8ac2a24b9367d62
Branimir <brani_svetoslavov@abv.bg>
2017-03-14 14:53:42 +0200

    Remove unused directives in file

--- PhotoLife/PhotoLife.Services/UserService.cs
+++ PhotoLife/PhotoLife.Services/UserService.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using PhotoLife.Data.Contracts;
 using PhotoLife.Models;
 using PhotoLife.Services.Contracts;
