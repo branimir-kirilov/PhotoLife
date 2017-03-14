f7edf315f00f45baa464a78a029eec8d71f0eec5
Branimir <brani_svetoslavov@abv.bg>
2017-03-14 19:07:34 +0200

    Add more  Account Controller Register() Tests

--- PhotoLife/PhotoLife.Web.Tests/Controllers/Account/After_Register_Should.cs
+++ PhotoLife/PhotoLife.Web.Tests/Controllers/Account/After_Register_Should.cs
@@ -181,5 +181,47 @@ namespace PhotoLife.Web.Tests.Controllers.Account
             Assert.IsInstanceOf<ViewResult>(res);
         }
 
+        [TestCase("fakeMail@fakeService.fakeDomain", "branimir", "fakeDescription", "fakePassword", "fakeUrl")]
+        public void _Set_ViewModel_Correctly_WhenIdentityResult_NotSuccess(
+            string email,
+            string name,
+            string description,
+            string password,
+            string profilePicUrl)
+        {
+            //Arrange
+            var mockedAuthenticationProvider = new Mock<IAuthenticationProvider>();
+            mockedAuthenticationProvider.Setup(
+                    p =>
+                        p.RegisterAndLoginUser(
+                            It.IsAny<User>(),
+                            It.IsAny<string>(),
+                            It.IsAny<bool>(),
+                            It.IsAny<bool>()))
+                .Returns(IdentityResult.Failed());
+
+            var user = new User();
+
+            var mockedFactory = new Mock<IUserFactory>();
+            mockedFactory.Setup(f => f.CreateUser(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+                 .Returns(user);
+
+            var model = new RegisterViewModel()
+            {
+                Email = email,
+                Name = name,
+                Password = password,
+                Description = description,
+                ProfilePicUrl = profilePicUrl
+            };
+
+            var controller = new AccountController(mockedAuthenticationProvider.Object, mockedFactory.Object);
+
+            //Act
+            var res = controller.Register(model) as ViewResult;
+
+            //Assert
+            Assert.AreSame(model, res.Model);
+        }
     }
 }
