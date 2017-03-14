1d8eeb523f2e21633057b3b9e4692a51d0c1332c
Branimir <brani_svetoslavov@abv.bg>
2017-03-14 20:40:18 +0200

    Add constructor tests for null argument

---
+++ PhotoLife/PhotoLife.Services.Tests/UserServiceTests/Constructor_Should.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+using PhotoLife.Data.Contracts;
+using PhotoLife.Models;
+
+namespace PhotoLife.Services.Tests.UserServiceTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+
+        [Test]
+        public void _Throw_ArgumentNullException_WhenRepository_IsNull()
+        {
+            //Arrange
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+
+            //Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new UserService(null, mockedUnitOfWork.Object));
+        }
+
+        [Test]
+        public void _Throw_ArgumentNullException_WhenUnitOfWork_IsNull()
+        {
+            //Arrange
+            var mockedUserRepository = new Mock<IRepository<User>>();
+
+            //Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new UserService(mockedUserRepository.Object, null));
+        }
+    }
+}
