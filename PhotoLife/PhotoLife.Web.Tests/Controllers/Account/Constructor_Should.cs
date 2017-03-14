87995797b5241fe63ebfd41facf0bb446a527ccf
Branimir <brani_svetoslavov@abv.bg>
2017-03-14 16:19:46 +0200

    Finish contructor tests for Account controller

--- PhotoLife/PhotoLife.Web.Tests/Controllers/Account/Constructor_Should.cs
+++ PhotoLife/PhotoLife.Web.Tests/Controllers/Account/Constructor_Should.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Web.Mvc;
 using Moq;
 using NUnit.Framework;
+using PhotoLife.Authentication.Providers;
 using PhotoLife.Factories;
 using PhotoLife.Controllers;
 
@@ -18,6 +20,58 @@ namespace PhotoLife.Web.Tests.Controllers.Account
             //Act & Assert 
             Assert.Throws<ArgumentNullException>(() => new AccountController(null, mockedFactory.Object));
 
+        }
+
+        [Test]
+        public void _Throw_ArgumentNullException_IfUserFactory_IsNull()
+        {
+            //Arrange
+            var mockedProvider = new Mock<IAuthenticationProvider>();
+
+            //Act & Assert 
+            Assert.Throws<ArgumentNullException>(() => new AccountController(mockedProvider.Object, null));
+
+        }
+
+        [Test]
+        public void _NotThrow_IfEveryting_PassedCorrectly()
+        {
+            //Arrange
+            var mockedProvider = new Mock<IAuthenticationProvider>();
+            var mockedFactory = new Mock<IUserFactory>();
+
+            //Act & Assert 
+            Assert.DoesNotThrow(() => new AccountController(mockedProvider.Object, mockedFactory.Object));
+
+        }
+
+
+        [Test]
+        public void _Initialize_NotNull_IfEveryting_PassedCorrectly()
+        {
+            //Arrange
+            var mockedProvider = new Mock<IAuthenticationProvider>();
+            var mockedFactory = new Mock<IUserFactory>();
+
+            //Act 
+            var controller = new AccountController(mockedProvider.Object, mockedFactory.Object);
+
+            //Assert
+            Assert.IsNotNull(controller);
+        }
+
+        [Test]
+        public void _Is_CorrectInstance_IfEveryting_PassedCorrectly()
+        {
+            //Arrange
+            var mockedProvider = new Mock<IAuthenticationProvider>();
+            var mockedFactory = new Mock<IUserFactory>();
+
+            //Act 
+            var controller = new AccountController(mockedProvider.Object, mockedFactory.Object);
+
+            //Assert
+            Assert.IsInstanceOf<Controller>(controller);
         }
     }
 }
