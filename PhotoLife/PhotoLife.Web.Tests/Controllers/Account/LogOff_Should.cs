009a17049e4b71617085efbbff6023a2fdbdec91
Branimir <brani_svetoslavov@abv.bg>
2017-03-14 16:32:58 +0200

    Finish log off tests

--- PhotoLife/PhotoLife.Web.Tests/Controllers/Account/LogOff_Should.cs
+++ PhotoLife/PhotoLife.Web.Tests/Controllers/Account/LogOff_Should.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 using Moq;
 using NUnit.Framework;
 using PhotoLife.Authentication.Providers;
@@ -28,6 +29,38 @@ namespace PhotoLife.Web.Tests.Controllers.Account
 
             //Assert
             mockedProvider.Verify(p => p.SignOut(), Times.Once);
+        }
+
+        [Test]
+        public void _Should_NotReturn_Null()
+        {
+            //Arrange
+            var mockedProvider = new Mock<IAuthenticationProvider>();
+            var mockedFactory = new Mock<IUserFactory>();
+
+            var controller = new AccountController(mockedProvider.Object, mockedFactory.Object);
+
+            //Act
+            var res = controller.LogOff();
+
+            //Assert
+            Assert.IsNotNull(res);
+        }
+
+        [Test]
+        public void _Should_Redirect_ToAction()
+        {
+            //Arrange
+            var mockedProvider = new Mock<IAuthenticationProvider>();
+            var mockedFactory = new Mock<IUserFactory>();
+
+            var controller = new AccountController(mockedProvider.Object, mockedFactory.Object);
+
+            //Act
+            var res = controller.LogOff();
+
+            //Assert
+            Assert.IsInstanceOf<RedirectToRouteResult>(res);
         }
     }
 }
