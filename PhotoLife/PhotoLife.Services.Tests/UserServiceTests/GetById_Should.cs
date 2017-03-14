3c04d4153273c07c9e1abb9a449d6f58a0dc49e9
Branimir <brani_svetoslavov@abv.bg>
2017-03-14 23:16:14 +0200

    Add user service GetById() Tests

---
+++ PhotoLife/PhotoLife.Services.Tests/UserServiceTests/GetById_Should.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using NUnit.Framework;
+using PhotoLife.Data.Contracts;
+using PhotoLife.Models;
+
+namespace PhotoLife.Services.Tests.UserServiceTests
+{
+    [TestFixture]
+    public class GetById_Should
+    {
+        [TestCase("some id")]
+        [TestCase("other id")]
+        public void _CallRepository_GetByIdMethod(string id)
+        {
+            //Arrange
+            var mockedRepository = new Mock<IRepository<User>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+
+            var userService = new UserService(mockedRepository.Object, mockedUnitOfWork.Object);
+
+            //Act
+            userService.GetUserById(id);
+
+            //Assert            
+            mockedRepository.Verify(r => r.GetById(id), Times.Once);
+        }
+    }
+}
