bc91f904388d2fd246b1be131c3b875688da2dad
Branimir <brani_svetoslavov@abv.bg>
2017-03-14 16:56:09 +0200

    Add classes for post login and register tests

---
+++ PhotoLife/PhotoLife.Web.Tests/Controllers/Account/After_Login_Should.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace PhotoLife.Web.Tests.Controllers.Account
+{
+    [TestFixture]
+    public class After_Login_Should
+    {
+        [Test]
+        public void Test()
+        {
+            
+        }
+    }
+}
---
+++ PhotoLife/PhotoLife.Web.Tests/Controllers/Account/After_Register_Should.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace PhotoLife.Web.Tests.Controllers.Account
+{
+    [TestFixture]
+    public class After_Register_Should
+    {
+        [Test]
+        public void Test()
+        {
+
+        }
+    }
+}
