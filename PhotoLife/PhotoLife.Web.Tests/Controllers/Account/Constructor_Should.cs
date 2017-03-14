9233478bd12f8dd494099df1c61e6da49ca760aa
Branimir <brani_svetoslavov@abv.bg>
2017-03-14 16:12:23 +0200

    Add references to tested projects

---
+++ PhotoLife/PhotoLife.Web.Tests/Controllers/Account/Constructor_Should.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+using PhotoLife.Factories;
+using PhotoLife.Controllers;
+
+namespace PhotoLife.Web.Tests.Controllers.Account
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void _Throw_ArgumentNullException_IfAuthenticationProvider_IsNull()
+        {
+            //Arrange
+            var mockedFactory = new Mock<IUserFactory>();
+
+            //Act & Assert 
+            Assert.Throws<NullReferenceException>(() => new AccountController(null, mockedFactory.Object));
+
+        }
+    }
+}
