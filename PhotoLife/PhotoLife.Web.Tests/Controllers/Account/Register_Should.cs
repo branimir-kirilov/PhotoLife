1a601cec667727030ea373123e82023fcf4918cd
Branimir <brani_svetoslavov@abv.bg>
2017-03-14 16:52:55 +0200

    Finished Register() Tests

---
+++ PhotoLife/PhotoLife.Web.Tests/Controllers/Account/Register_Should.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Moq;
+using NUnit.Framework;
+using PhotoLife.Authentication.Providers;
+using PhotoLife.Controllers;
+using PhotoLife.Factories;
+
+namespace PhotoLife.Web.Tests.Controllers.Account
+{
+    [TestFixture]
+    public class Register_Should
+    {
+        [Test]
+        public void _Return_NotNull()
+        {
+            //Arrange
+            var mockedProvider = new Mock<IAuthenticationProvider>();
+            var mockedFactory = new Mock<IUserFactory>();
+
+            var controller = new AccountController(mockedProvider.Object, mockedFactory.Object);
+
+            //Act
+            var result = controller.Register();
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void _Return_View()
+        {
+            //Arrange
+            var mockedProvider = new Mock<IAuthenticationProvider>();
+            var mockedFactory = new Mock<IUserFactory>();
+
+            var controller = new AccountController(mockedProvider.Object, mockedFactory.Object);
+
+            //Act
+            var result = controller.Register();
+
+            //Assert
+            Assert.IsInstanceOf<ViewResult>(result);
+        }
+    }
+}
