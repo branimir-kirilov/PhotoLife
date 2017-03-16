f73103493b663926b0fd7004c2c752d6d5e3f025
Branimir <brani_svetoslavov@abv.bg>
2017-03-16 12:53:35 +0200

    Finish UserModel properties tests

---
+++ PhotoLife/PhotoLife.Models.Tests/UserModelTests/Property_.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace PhotoLife.Models.Tests.UserModelTests
+{
+    [TestFixture]
+    public class Property_
+    {
+        [Test]
+        public void _Name_Should_SetName_Correctly()
+        {
+            //Arrange
+            var name = "me";
+            var user = new User();
+
+            //Act 
+            user.Name = name;
+
+            //Assert  
+            Assert.AreEqual(name, user.Name);
+        }
+
+        [Test]
+        public void _Description_Should_Description_Correctly()
+        {
+            //Arrange
+            var description = "I am me";
+            var user = new User();
+
+            //Act 
+            user.Description = description;
+
+            //Assert  
+            Assert.AreEqual(description, user.Description);
+        }
+
+        [Test]
+        public void _ProfilePicUrl_Should_SetProfilePicUrl_Correctly()
+        {
+            //Arrange
+            var profilePicUrl = "cloudinary.com/somecloud/somepic";
+            var user = new User();
+
+            //Act 
+            user.ProfilePicUrl = profilePicUrl;
+
+            //Assert  
+            Assert.AreEqual(profilePicUrl, user.ProfilePicUrl);
+        }
+    }
+}
