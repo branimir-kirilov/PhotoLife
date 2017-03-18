93faa9947dc47ef1b46d2e86df442c0b0c2b1b9a
Branimir <brani_svetoslavov@abv.bg>
2017-03-18 23:36:33 +0200

    Fix naming

--- PhotoLife/PhotoLife.Web/Models/AccountViewModels.cs
+++ PhotoLife/PhotoLife.Web/Models/AccountViewModels.cs
@@ -38,15 +38,15 @@ namespace PhotoLife.Models
         [Display(Name = "Email")]
         public string Email { get; set; }
 
+
         [Required]
+        [Display(Name = "Username")]
+        [StringLength(60, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        public string UserName { get; set; }
+        
         [Display(Name = "Name")]
         [StringLength(60, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         public string Name { get; set; }
-
-        [Required]
-        [Display(Name = "Name")]
-        [StringLength(60, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
-        public string UserName { get; set; }
 
         [Required]
         [Display(Name = "Description")]
