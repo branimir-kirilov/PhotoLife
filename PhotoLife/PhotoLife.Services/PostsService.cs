091a671fa37dab287e854b7b561c0dbe885e3fe9
Branimir <brani_svetoslavov@abv.bg>
2017-03-21 00:00:07 +0200

    Add posts service

---
+++ PhotoLife/PhotoLife.Services/PostsService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PhotoLife.Data.Contracts;
+using PhotoLife.Models;
+using PhotoLife.Services.Contracts;
+
+namespace PhotoLife.Services
+{
+    public class PostsService: IPostService
+    {
+        private readonly IRepository<Post> postsRepository;
+        private readonly IUnitOfWork unitOfWork;
+
+        public PostsService(
+            IRepository<Post> postsRepository, 
+            IUnitOfWork unitOfWork)
+        {
+            if (postsRepository == null)
+            {
+                throw new ArgumentNullException("postsRepository");
+            }
+
+            if (unitOfWork == null)
+            {
+                throw new ArgumentNullException("unitOfWorks");
+            }
+
+            this.postsRepository = postsRepository;
+            this.unitOfWork = unitOfWork;
+        }
+        public Post GetPostById(string id)
+        {
+            return this.postsRepository.GetById(id);
+        }
+
+
+    }
+}
