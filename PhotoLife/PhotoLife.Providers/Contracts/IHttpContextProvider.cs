85cd56d75c0e89e797e7c3aa99f579ce0f0fe3fd
Branimir <brani_svetoslavov@abv.bg>
2017-03-21 15:31:10 +0200

    Add httpcontextprovider

--- PhotoLife/PhotoLife.Providers/Contracts/IHttpContextProvider.cs
+++ PhotoLife/PhotoLife.Providers/Contracts/IHttpContextProvider.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Security.Principal;
+using System.Web;
+using Microsoft.Owin;
 
 namespace PhotoLife.Providers.Contracts
 {
     public interface IHttpContextProvider
     {
+        HttpContext CurrentHttpContext { get; }
+
+        IOwinContext CurrentOwinContext { get; }
+
+        IIdentity CurrentIdentity { get; }
+        TManager GetUserManager<TManager>();
     }
 }
---
+++ PhotoLife/PhotoLife.Providers/HttpContextProvider.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Principal;
+using System.Web;
+using Microsoft.Owin;
+using Microsoft.AspNet.Identity.Owin;
+using PhotoLife.Providers.Contracts;
+
+namespace PhotoLife.Providers
+{
+    public class HttpContextProvider : IHttpContextProvider
+    {
+        public HttpContext CurrentHttpContext
+        {
+            get { return HttpContext.Current; }
+        }
+
+        public IOwinContext CurrentOwinContext
+        {
+            get { return this.CurrentHttpContext.GetOwinContext(); }
+        }
+
+        public IIdentity CurrentIdentity
+        {
+            get { return this.CurrentHttpContext.User.Identity; }
+        }
+
+        public TManager GetUserManager<TManager>()
+        {
+            return this.CurrentOwinContext.GetUserManager<TManager>();
+        }
+    }
+}
