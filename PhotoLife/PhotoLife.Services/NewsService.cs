25dee6479ec70ebb544dd8598e19647f208744d3
Branimir <brani_svetoslavov@abv.bg>
2017-03-21 11:40:36 +0200

    Add news service

---
+++ PhotoLife/PhotoLife.Services/NewsService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PhotoLife.Data.Contracts;
+using PhotoLife.Models;
+using PhotoLife.Services.Contracts;
+
+namespace PhotoLife.Services
+{
+    public class NewsService : INewsService
+    {
+        private readonly IRepository<News> newsRepository;
+        private readonly IUnitOfWork unitOfWork;
+
+        public NewsService(
+            IRepository<News> newsRepository,
+            IUnitOfWork unitOfWork)
+        {
+            if (newsRepository == null)
+            {
+                throw new ArgumentNullException("newsRepository");
+            }
+
+            if (unitOfWork == null)
+            {
+                throw new ArgumentNullException("unitOfWorks");
+            }
+
+            this.newsRepository = newsRepository;
+            this.unitOfWork = unitOfWork;
+        }
+
+        public News GetNewsById(string id)
+        {
+            return this.newsRepository.GetById(id);
+        }
+
+        public IEnumerable<News> GetTopPosts(int countOfPosts)
+        {
+            var res =
+                this.newsRepository.GetAll(
+                    (News news) => true,
+                    (News news) => news.Views, true)
+                    .Take(countOfPosts);
+
+            return res;
+        }
+    }
+}
