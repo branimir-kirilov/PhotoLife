eaeb20f9a87911e9546f75232e87ef677b7cb7d1
Branimir <brani_svetoslavov@abv.bg>
2017-03-21 11:30:15 +0200

    Add posts controller

---
+++ PhotoLife/PhotoLife.Web/Controllers/PostController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhotoLife.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: AddPost
+        public ActionResult AddPost()
+        {
+            return View();
+        }
+    }
+}
