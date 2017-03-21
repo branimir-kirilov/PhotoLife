e0a1b3b7911147506c4d1762a896a07a12316258
Branimir <brani_svetoslavov@abv.bg>
2017-03-21 16:07:39 +0200

    Add addPostViewModel

--- PhotoLife/PhotoLife.Web/Controllers/PostController.cs
+++ PhotoLife/PhotoLife.Web/Controllers/PostController.cs
@@ -1,20 +1,27 @@
 ﻿using System.Web.Mvc;
+using PhotoLife.Models.Post;
 
 namespace PhotoLife.Controllers
 {
     public class PostController : Controller
     {
         // Get: All
+        [AllowAnonymous]
         public ActionResult All()
         {
             return View();
         }
 
         // Post: Add
-        [Authorize]
+        [AllowAnonymous]
         public ActionResult Add()
         {
             return View();
+        }
+
+        public ActionResult Add(AddPostViewModel model)
+        {
+            
         }
     }
 }
---
+++ PhotoLife/PhotoLife.Web/Models/Post/AddPostViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CloudinaryDotNet;
+
+namespace PhotoLife.Models.Post
+{
+    public class AddPostViewModel
+    {
+        [Required]
+        [Display(Name = "Title")]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 2)]
+        public string Title { get; set; }
+
+        [Required]
+        [Display(Name = "Description")]
+        [StringLength(350, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 0)]
+        public string Description { get; set; }
+
+        public string ProfilePicUrl { get; set; }
+        public Category Category { get; set; }
+        
+        public Cloudinary Cloudinary { get; set; } 
+
+    }
+}
