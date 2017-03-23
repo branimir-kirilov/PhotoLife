e43a36e54957e4326a02dac68e9dfbeaa24e8bd7
Branimir <brani_svetoslavov@abv.bg>
2017-03-23 11:45:58 +0200

    Add getCurrentId tests

---
+++ PhotoLife/PhotoLife.Authentication.Tests/GetCurrentUserId_Should.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using PhotoLife.Providers.Contracts;
+
+namespace PhotoLife.Authentication.Tests
+{
+    [TestFixture]
+    public class GetCurrentUserId_Should
+    {
+        [Test]
+        public void _Call_HttpContextProviderCurrentIdentity()
+        {
+            //Arrange
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+
+            var mockedIdentity = new Mock<IIdentity>();
+
+            var mockedHttpContextProvider = new Mock<IHttpContextProvider>();
+            mockedHttpContextProvider.Setup(provider => provider.CurrentIdentity).Returns(mockedIdentity.Object);
+
+            var httpContextAuthProvider = new Providers.HttpContextAuthenticationProvider(mockedHttpContextProvider.Object,
+                mockedDateTimeProvider.Object);
+            
+            //Act
+            var result = httpContextAuthProvider.CurrentUserId;
+
+            //Assert
+            mockedHttpContextProvider.Verify(provider => provider.CurrentIdentity, Times.Once);
+        }
+    }
+}
