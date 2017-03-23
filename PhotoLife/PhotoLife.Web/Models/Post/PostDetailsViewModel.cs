e127f17f49a816f5f686eeb194b1a76ebfd2ce7c
Branimir <brani_svetoslavov@abv.bg>
2017-03-23 12:58:03 +0200

    Add post details view model

---
+++ PhotoLife/PhotoLife.Web/Models/Post/PostDetailsViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PhotoLife.Models.Enums;
+
+namespace PhotoLife.ViewModels.Post
+{
+    public class PostDetailsViewModel
+    {
+        public PostDetailsViewModel(Models.Post post)
+        {
+            this.Id = post.PostId;
+            this.Title = post.Title;
+            this.Description = post.Description;
+            this.ImageUrl = post.ImageUrl;
+            this.DatePublished = post.DatePublished;
+            this.Votes = post.Votes;
+            this.Category = post.Category.Name;
+        }
+
+        public PostDetailsViewModel()
+        {
+
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime DatePublished { get; set; }
+        public int Votes { get; set; }
+        public CategoryEnum Category { get; set; }
+    }
+}
