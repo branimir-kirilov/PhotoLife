325e322d4d9b8ef6ee795809098fc7cde9bc5801
Branimir <brani_svetoslavov@abv.bg>
2017-03-23 11:32:30 +0200

    Add constructor tests

---
+++ PhotoLife/PhotoLife.Authentication.Tests/HttpContextAuthenticationProvider/Constructor_Should.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+using PhotoLife.Providers.Contracts;
+
+namespace PhotoLife.Authentication.Tests.HttpContextAuthenticationProvider
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void _Throw_ArgumentNullException_WhenIHttpContextProvider_IsNull()
+        {
+            //Arrange & Act & Assert
+            Assert.Throws<ArgumentNullException>(
+                () => new Providers.HttpContextAuthenticationProvider(null));
+        }
+
+        [Test]
+        public void _NotThrow_ArgumentNullException_WhenIHttpContextProvider_IsCorrect()
+        {
+            //Arrange 
+            var mockedProvider = new Mock<IHttpContextProvider>();
+
+            //Act & Assert
+            Assert.DoesNotThrow(
+                () => new Providers.HttpContextAuthenticationProvider(mockedProvider.Object));
+        }
+
+        [Test]
+        public void _Initilize_NotNull_WhenIHttpContextProvider_IsCorrect()
+        {
+            //Arrange 
+            var mockedProvider = new Mock<IHttpContextProvider>();
+
+            //Act
+            var provider = new Providers.HttpContextAuthenticationProvider(mockedProvider.Object);
+
+            //Assert
+            Assert.IsNotNull(provider);
+        }
+
+        [Test]
+        public void _Intialize_CorrectInstance_WhenIHttpContextProvider_IsCorrect()
+        {
+            //Arrange 
+            var mockedProvider = new Mock<IHttpContextProvider>();
+
+            //Act
+            var provider = new Providers.HttpContextAuthenticationProvider(mockedProvider.Object);
+
+            //Assert
+            Assert.IsInstanceOf<Providers.HttpContextAuthenticationProvider>(provider);
+        }
+
+    }
+}
