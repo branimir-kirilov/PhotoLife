20e497e6d5e2ef3f42fb7b5b1b0bea5b722094ef
Branimir <brani_svetoslavov@abv.bg>
2017-03-23 18:52:29 +0200

    Add some more edit news tests. This time finieshed. For real

--- PhotoLife/PhotoLife.Services.Tests/NewsServiceTests/EditNews_Should.cs
+++ PhotoLife/PhotoLife.Services.Tests/NewsServiceTests/EditNews_Should.cs
@@ -110,7 +110,6 @@ namespace PhotoLife.Services.Tests.NewsServiceTests
 
         [TestCase(7, "title", "some text", "some.url/somepic/2", CategoryEnum.Abstract)]
         [TestCase(7, "title", "<h1>some text</h1>", "some.url/somepic/2", CategoryEnum.Celebrity)]
-
         public void _Call_UnitOfWork_Commit(int id, string title, string text, string imageUrl, CategoryEnum categoryEnum)
         {
             //Arrange
@@ -142,6 +141,146 @@ namespace PhotoLife.Services.Tests.NewsServiceTests
 
             //Assert
             mockedUnitOfWork.Verify(r => r.Commit(), Times.Once);
+        }
+
+        [TestCase(7, "title", "some text", "some.url/somepic/2", CategoryEnum.Abstract)]
+        [TestCase(7, "title", "<h1>some text</h1>", "some.url/somepic/2", CategoryEnum.Celebrity)]
+        public void _Should_Set_Title_Correctly(int id, string title, string text, string imageUrl, CategoryEnum categoryEnum)
+        {
+            //Arrange
+            var news = new News();
+            var category = new Category();
+
+            var mockedNewsRepository = new Mock<IRepository<News>>();
+            mockedNewsRepository.Setup(r => r.GetById(It.IsAny<int>())).Returns(news);
+
+            var mockedUserService = new Mock<IUserService>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedNewsFactory = new Mock<INewsFactory>();
+
+            var mockedCategoryService = new Mock<ICategoryService>();
+            mockedCategoryService.Setup(m => m.GetCategoryByName(It.IsAny<CategoryEnum>())).Returns(category);
+
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+
+            var newsService = new NewsService(
+                  mockedNewsRepository.Object,
+                  mockedUserService.Object,
+                  mockedUnitOfWork.Object,
+                  mockedNewsFactory.Object,
+                  mockedCategoryService.Object,
+                  mockedDateTimeProvider.Object);
+
+            //Act
+            newsService.EditNews(id, title, text, imageUrl, categoryEnum);
+
+            //Assert
+            Assert.AreEqual(news.Title, title);
+        }
+
+        [TestCase(7, "title", "some text", "some.url/somepic/2", CategoryEnum.Abstract)]
+        [TestCase(7, "title", "<h1>some text</h1>", "some.url/somepic/2", CategoryEnum.Celebrity)]
+        public void _Should_Set_Text_Correctly(int id, string title, string text, string imageUrl, CategoryEnum categoryEnum)
+        {
+            //Arrange
+            var news = new News();
+            var category = new Category();
+
+            var mockedNewsRepository = new Mock<IRepository<News>>();
+            mockedNewsRepository.Setup(r => r.GetById(It.IsAny<int>())).Returns(news);
+
+            var mockedUserService = new Mock<IUserService>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedNewsFactory = new Mock<INewsFactory>();
+
+            var mockedCategoryService = new Mock<ICategoryService>();
+            mockedCategoryService.Setup(m => m.GetCategoryByName(It.IsAny<CategoryEnum>())).Returns(category);
+
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+
+            var newsService = new NewsService(
+                  mockedNewsRepository.Object,
+                  mockedUserService.Object,
+                  mockedUnitOfWork.Object,
+                  mockedNewsFactory.Object,
+                  mockedCategoryService.Object,
+                  mockedDateTimeProvider.Object);
+
+            //Act
+            newsService.EditNews(id, title, text, imageUrl, categoryEnum);
+
+            //Assert
+            Assert.AreEqual(news.Text, text);
+        }
+
+        [TestCase(7, "title", "some text", "some.url/somepic/2", CategoryEnum.Abstract)]
+        [TestCase(7, "title", "<h1>some text</h1>", "some.url/somepic/2", CategoryEnum.Celebrity)]
+        public void _Should_Set_Category_Correctly(int id, string title, string text, string imageUrl, CategoryEnum categoryEnum)
+        {
+            //Arrange
+            var news = new News();
+            var category = new Category();
+
+            var mockedNewsRepository = new Mock<IRepository<News>>();
+            mockedNewsRepository.Setup(r => r.GetById(It.IsAny<int>())).Returns(news);
+
+            var mockedUserService = new Mock<IUserService>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedNewsFactory = new Mock<INewsFactory>();
+
+            var mockedCategoryService = new Mock<ICategoryService>();
+            mockedCategoryService.Setup(m => m.GetCategoryByName(It.IsAny<CategoryEnum>())).Returns(category);
+
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+
+            var newsService = new NewsService(
+                  mockedNewsRepository.Object,
+                  mockedUserService.Object,
+                  mockedUnitOfWork.Object,
+                  mockedNewsFactory.Object,
+                  mockedCategoryService.Object,
+                  mockedDateTimeProvider.Object);
+
+            //Act
+            newsService.EditNews(id, title, text, imageUrl, categoryEnum);
+
+            //Assert
+            Assert.AreSame(news.Category, category);
+        }
+
+        [TestCase(7, "title", "some text", "some.url/somepic/2", CategoryEnum.Abstract)]
+        [TestCase(7, "title", "<h1>some text</h1>", "some.url/somepic/2", CategoryEnum.Celebrity)]
+        public void _Should_Set_ImageUrl_Correctly(int id, string title, string text, string imageUrl, CategoryEnum categoryEnum)
+        {
+            //Arrange
+            var news = new News();
+            var category = new Category();
+
+            var mockedNewsRepository = new Mock<IRepository<News>>();
+            mockedNewsRepository.Setup(r => r.GetById(It.IsAny<int>())).Returns(news);
+
+            var mockedUserService = new Mock<IUserService>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedNewsFactory = new Mock<INewsFactory>();
+
+            var mockedCategoryService = new Mock<ICategoryService>();
+            mockedCategoryService.Setup(m => m.GetCategoryByName(It.IsAny<CategoryEnum>())).Returns(category);
+
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+
+            var newsService = new NewsService(
+                  mockedNewsRepository.Object,
+                  mockedUserService.Object,
+                  mockedUnitOfWork.Object,
+                  mockedNewsFactory.Object,
+                  mockedCategoryService.Object,
+                  mockedDateTimeProvider.Object);
+
+            //Act
+            newsService.EditNews(id, title, text, imageUrl, categoryEnum);
+
+            //Assert
+            Assert.AreEqual(news.ImageUrl, imageUrl);
         }
     }
 }
