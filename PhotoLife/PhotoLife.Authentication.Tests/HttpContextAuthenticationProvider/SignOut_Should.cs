5db211c6a885d7b203f1c17e0057f56fdd6df30b
Branimir <brani_svetoslavov@abv.bg>
2017-03-23 12:42:28 +0200

    Finish sign out tests

---
+++ PhotoLife/PhotoLife.Authentication.Tests/HttpContextAuthenticationProvider/SignOut_Should.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Moq;
+using NUnit.Framework;
+using PhotoLife.Providers.Contracts;
+
+namespace PhotoLife.Authentication.Tests.HttpContextAuthenticationProvider
+{
+    [TestFixture]
+    public class SignOut_Should
+    {
+        [Test]
+        public void _Call_HttpContextProvider_CurrentOwinContext()
+        {
+            //Arrange
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+
+            var mockedAuthenticationManager = new Mock<IAuthenticationManager>();
+
+            var mockedOwinCtx = new Mock<IOwinContext>();
+            mockedOwinCtx.Setup(ctx => ctx.Authentication).Returns(mockedAuthenticationManager.Object);
+
+            var mockedHttpContextProvider = new Mock<IHttpContextProvider>();
+            mockedHttpContextProvider.Setup(p => p.CurrentOwinContext)
+                .Returns(mockedOwinCtx.Object);
+
+            var provider = new Providers.HttpContextAuthenticationProvider(mockedHttpContextProvider.Object, mockedDateTimeProvider.Object);
+
+            //Act
+            provider.SignOut();
+
+
+            //Assert
+            mockedHttpContextProvider.Verify(m => m.CurrentOwinContext, Times.Once);
+
+        }
+
+        [Test]
+        public void _Call_AuthenticationManager_CurrentOwinContext()
+        {
+            //Arrange
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+
+            var mockedAuthenticationManager = new Mock<IAuthenticationManager>();
+
+            var mockedOwinCtx = new Mock<IOwinContext>();
+            mockedOwinCtx.Setup(ctx => ctx.Authentication).Returns(mockedAuthenticationManager.Object);
+
+            var mockedHttpContextProvider = new Mock<IHttpContextProvider>();
+            mockedHttpContextProvider.Setup(p => p.CurrentOwinContext)
+                .Returns(mockedOwinCtx.Object);
+
+            var provider = new Providers.HttpContextAuthenticationProvider(mockedHttpContextProvider.Object, mockedDateTimeProvider.Object);
+
+            //Act
+            provider.SignOut();
+
+
+            //Assert
+            mockedAuthenticationManager.Verify(m => m.SignOut(It.IsAny<string>()), Times.Once);
+
+        }
+    }
+}
