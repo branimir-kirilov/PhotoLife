5af0cb4117a46fa1b1065395b65d12cef8e8ede4
Branimir <brani_svetoslavov@abv.bg>
2017-03-23 18:03:17 +0200

    Finish create news tests

--- PhotoLife/PhotoLife.Services.Tests/NewsServiceTests/CreateNews_Should.cs
+++ PhotoLife/PhotoLife.Services.Tests/NewsServiceTests/CreateNews_Should.cs
@@ -20,8 +20,8 @@ namespace PhotoLife.Services.Tests.NewsServiceTests
         [TestCase("userid8", "Title", "text of the news which is quite big actually fuck this", "www.cloudinary.net/somePic9", CategoryEnum.Celebrity)]
         [TestCase("userid7", "Title", "<h1>text</h1>", "www.cloudinary.net/somePic9", CategoryEnum.Celebrity)]
         public void _Call_UserService_GetById(
-            string userId, 
-            string title, 
+            string userId,
+            string title,
             string text,
             string coverPicture,
             CategoryEnum categoryEnum)
@@ -154,7 +154,7 @@ namespace PhotoLife.Services.Tests.NewsServiceTests
             newsService.CreateNews(userId, title, text, coverPicture, categoryEnum);
 
             //Assert
-            mockedNewsFactory.Verify(c => c.CreateNews(title, text ,coverPicture, user, category, date), Times.Once);
+            mockedNewsFactory.Verify(c => c.CreateNews(title, text, coverPicture, user, category, date), Times.Once);
         }
 
         [TestCase("userid8", "Title", "text of the news which is quite big actually fuck this", "www.cloudinary.net/somePic9", CategoryEnum.Celebrity)]
@@ -198,6 +198,95 @@ namespace PhotoLife.Services.Tests.NewsServiceTests
 
             //Assert
             mockedNewsRepository.Verify(c => c.Add(news), Times.Once);
+        }
+
+        [TestCase("userid8", "Title", "text of the news which is quite big actually fuck this", "www.cloudinary.net/somePic9", CategoryEnum.Celebrity)]
+        [TestCase("userid7", "Title", "<h1>text</h1>", "www.cloudinary.net/somePic9", CategoryEnum.Celebrity)]
+        public void _Call_UnitOfWork_Commit(
+        string userId,
+        string title,
+        string text,
+        string coverPicture,
+        CategoryEnum categoryEnum)
+        {
+            //Arrange
+            var user = new User();
+            var date = new DateTime();
+            var category = new Category();
+
+            var mockedNewsRepository = new Mock<IRepository<News>>();
+
+            var mockedUserService = new Mock<IUserService>();
+            mockedUserService.Setup(m => m.GetUserById(It.IsAny<string>())).Returns(user);
+
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedNewsFactory = new Mock<INewsFactory>();
+
+            var mockedCategoryService = new Mock<ICategoryService>();
+            mockedCategoryService.Setup(c => c.GetCategoryByName(categoryEnum)).Returns(category);
+
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+            mockedDateTimeProvider.Setup(d => d.GetCurrentDate()).Returns(date);
+
+            var newsService = new NewsService(
+                  mockedNewsRepository.Object,
+                  mockedUserService.Object,
+                  mockedUnitOfWork.Object,
+                  mockedNewsFactory.Object,
+                  mockedCategoryService.Object,
+                  mockedDateTimeProvider.Object);
+
+            //Act
+            var news = newsService.CreateNews(userId, title, text, coverPicture, categoryEnum);
+
+            //Assert
+            mockedUnitOfWork.Verify(u => u.Commit(), Times.Once);
+        }
+
+        [TestCase("userid8", "Title", "text of the news which is quite big actually fuck this", "www.cloudinary.net/somePic9", CategoryEnum.Celebrity)]
+        [TestCase("userid7", "Title", "<h1>text</h1>", "www.cloudinary.net/somePic9", CategoryEnum.Celebrity)]
+        public void _Call_ReturnCorrect_News_Commit(
+           string userId,
+           string title,
+           string text,
+           string coverPicture,
+           CategoryEnum categoryEnum)
+        {
+            //Arrange
+            var user = new User();
+            var date = new DateTime();
+            var category = new Category();
+            var news = new News();
+
+            var mockedNewsRepository = new Mock<IRepository<News>>();
+
+            var mockedUserService = new Mock<IUserService>();
+            mockedUserService.Setup(m => m.GetUserById(It.IsAny<string>())).Returns(user);
+
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+
+            var mockedNewsFactory = new Mock<INewsFactory>();
+            mockedNewsFactory.Setup(f => f.CreateNews(title, text, coverPicture, user, category, date)).Returns(news);
+            
+            var mockedCategoryService = new Mock<ICategoryService>();
+            mockedCategoryService.Setup(c => c.GetCategoryByName(categoryEnum)).Returns(category);
+            
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+            mockedDateTimeProvider.Setup(d => d.GetCurrentDate()).Returns(date);
+
+            var newsService = new NewsService(
+                  mockedNewsRepository.Object,
+                  mockedUserService.Object,
+                  mockedUnitOfWork.Object,
+                  mockedNewsFactory.Object,
+                  mockedCategoryService.Object,
+                  mockedDateTimeProvider.Object);
+
+            //Act
+            var newsRes = newsService.CreateNews(userId, title, text, coverPicture, categoryEnum);
+
+            //Assert
+            Assert.AreSame(news, newsRes);
         }
     }
 }
