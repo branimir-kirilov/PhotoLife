36e1b0f8d2ba16289a771707d16649d3fca252f4
Branimir <brani_svetoslavov@abv.bg>
2017-03-24 13:40:11 +0200

    Finish category service constructor tests

--- PhotoLife/PhotoLife.Services.Tests/CategoryServiceTests/Constructor_Should.cs
+++ PhotoLife/PhotoLife.Services.Tests/CategoryServiceTests/Constructor_Should.cs
@@ -1,12 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using PhotoLife.Data.Contracts;
+using PhotoLife.Factories;
+using PhotoLife.Models;
 
 namespace PhotoLife.Services.Tests.CategoryServiceTests
 {
-    class Constructor_Should
+    [TestFixture]
+    public class Constructor_Should
     {
+        [Test]
+        public void _Initialize_Correctly_WhenEverythingPassed_Valid()
+        {
+            //Arrange
+            var mockedCategoryRepository = new Mock<IRepository<Category>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedCategoryFactory = new Mock<ICategoryFactory>();
+
+            //Act
+            var categoryService = new CategoryService(mockedCategoryRepository.Object, mockedUnitOfWork.Object, mockedCategoryFactory.Object);
+
+            //Assert & Assert
+            Assert.IsNotNull(categoryService);
+        }
+
+        [Test]
+        public void _Throw_ArgumentNullException_When_Repository_IsNull()
+        {
+            //Arrange
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedCategoryFactory = new Mock<ICategoryFactory>();
+
+            //Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new CategoryService(null, mockedUnitOfWork.Object, mockedCategoryFactory.Object));
+        }
+
+        [Test]
+        public void _Throw_ArgumentNullException_When_UnitOfWork_IsNull()
+        {
+            //Arrange
+            var mockedCategoryRepository = new Mock<IRepository<Category>>();
+            var mockedCategoryFactory = new Mock<ICategoryFactory>();
+
+            //Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new CategoryService(mockedCategoryRepository.Object, null, mockedCategoryFactory.Object));
+        }
+
+        [Test]
+        public void _Throw_ArgumentNullException_When_CategoryFactory_IsNull()
+        {
+            //Arrange
+            var mockedCategoryRepository = new Mock<IRepository<Category>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+
+            //Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new CategoryService(mockedCategoryRepository.Object, mockedUnitOfWork.Object, null));
+        }
     }
 }
