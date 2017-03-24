28c70d04a8f0e2b4301ae550abefe92abb419c7a
Branimir <brani_svetoslavov@abv.bg>
2017-03-24 14:10:59 +0200

    Finish CategoryService tests

---
+++ PhotoLife/PhotoLife.Services.Tests/CategoryServiceTests/GetAll_Should.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using PhotoLife.Data.Contracts;
+using PhotoLife.Factories;
+using PhotoLife.Models;
+using PhotoLife.Models.Enums;
+
+namespace PhotoLife.Services.Tests.CategoryServiceTests
+{
+    [TestFixture]
+    public class GetAll_Should
+    {
+        [TestCase(CategoryEnum.Abstract)]
+        [TestCase(CategoryEnum.City)]
+        public void _Call_Repository_GetAll(CategoryEnum categoryEnum)
+        {
+            //Arrange            
+            var mockedCategoryRepository = new Mock<IRepository<Category>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedCategoryFactory = new Mock<ICategoryFactory>();
+
+            var categoryService = new CategoryService(mockedCategoryRepository.Object, mockedUnitOfWork.Object,
+                mockedCategoryFactory.Object);
+
+            //Act
+            categoryService.GetCategoryByName(categoryEnum);
+
+            //Assert
+            mockedCategoryRepository.Verify(r => r.GetAll, Times.Once);
+        }
+
+        [TestCase(CategoryEnum.Abstract)]
+        [TestCase(CategoryEnum.City)]
+        public void _Return_Correctly(CategoryEnum categoryEnum)
+        {
+            //Arrange
+            var category = new Category();
+            category.Name = categoryEnum;
+            var categoryList = new List<Category>()
+            {
+                category,
+            }.AsQueryable();
+
+            var mockedCategoryRepository = new Mock<IRepository<Category>>();
+            mockedCategoryRepository.Setup(r => r.GetAll).Returns(categoryList);
+
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedCategoryFactory = new Mock<ICategoryFactory>();
+
+            var categoryService = new CategoryService(mockedCategoryRepository.Object, mockedUnitOfWork.Object,
+                mockedCategoryFactory.Object);
+
+            //Act
+            var res = categoryService.GetAll();
+
+            //Assert
+            CollectionAssert.AreEqual(categoryList, res);
+        }
+    }
+}
