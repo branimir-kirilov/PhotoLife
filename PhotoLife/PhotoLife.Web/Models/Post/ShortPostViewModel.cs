62964a16a201ca6947223e9ddd47f9169b6362c2
Branimir <brani_svetoslavov@abv.bg>
2017-03-26 18:43:02 +0300

    Add short post view model

---
+++ PhotoLife/PhotoLife.Web/Models/Post/ShortPostViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhotoLife.ViewModels.Post
+{
+    public class ShortPostViewModel
+    {
+        public ShortPostViewModel(Models.Post post)
+        {
+            this.Id = post.PostId;
+            this.Title = post.Title;
+            this.ImageUrl = post.ImageUrl;
+            this.DatePublished = post.DatePublished;
+            this.Votes = post.Votes;
+        }
+
+        public ShortPostViewModel()
+        {
+                
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime DatePublished { get; set; }
+        public int Votes { get; set; }
+    }
+}
