9505f9bf78f61d8e7565f0adfaf1e45e68aad7b2
Branimir <brani_svetoslavov@abv.bg>
2017-03-27 02:26:55 +0300

    Add news service comment tests

---
+++ PhotoLife/PhotoLife.Services.Tests/NewsServiceTests/AddComment_Should.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using PhotoLife.Data.Contracts;
+using PhotoLife.Factories;
+using PhotoLife.Models;
+using PhotoLife.Providers.Contracts;
+using PhotoLife.Services.Contracts;
+
+namespace PhotoLife.Services.Tests.NewsServiceTests
+{
+    [TestFixture]
+    public class AddComment_Should
+    {
+        [TestCase(7)]
+        public void _Call_NewsRepository_GetById(int id)
+        {
+            //Arrange
+            var mockedNewsRepository = new Mock<IRepository<News>>();
+            var mockedUserService = new Mock<IUserService>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedNewsFactory = new Mock<INewsFactory>();
+            var mockedCategoryService = new Mock<ICategoryService>();
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+
+            var newsService = new NewsService(
+                  mockedNewsRepository.Object,
+                  mockedUserService.Object,
+                  mockedUnitOfWork.Object,
+                  mockedNewsFactory.Object,
+                  mockedCategoryService.Object,
+                  mockedDateTimeProvider.Object);
+
+            var comment = new Comment();
+            
+            //Act
+            newsService.AddComment(id, comment);
+
+            //Assert
+            mockedNewsRepository.Verify(r => r.GetById(id), Times.Once);
+
+        }
+
+        [TestCase(7)]
+        public void _Call_UnitOfWorkCommit_GetById(int id)
+        {
+            //Arrange
+            var mockedNewsRepository = new Mock<IRepository<News>>();
+            var mockedUserService = new Mock<IUserService>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedNewsFactory = new Mock<INewsFactory>();
+            var mockedCategoryService = new Mock<ICategoryService>();
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+
+            var newsService = new NewsService(
+                  mockedNewsRepository.Object,
+                  mockedUserService.Object,
+                  mockedUnitOfWork.Object,
+                  mockedNewsFactory.Object,
+                  mockedCategoryService.Object,
+                  mockedDateTimeProvider.Object);
+
+            var comment = new Comment();
+
+            //Act
+            newsService.AddComment(id, comment);
+
+            //Assert
+            mockedUnitOfWork.Verify(u => u.Commit(), Times.Once);
+
+        }
+    }
+}
