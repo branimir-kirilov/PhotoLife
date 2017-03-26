da320063be67fcb3d8e992666aba025434b5c53b
Branimir <brani_svetoslavov@abv.bg>
2017-03-27 02:19:00 +0300

    Add area

---
+++ PhotoLife/PhotoLife.Web/Area/Admin/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace PhotoLife.Area.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        [Authorize(Roles = "Administrator")]
+        public class AdministrationController : Controller
+        {
+            public ActionResult Index()
+            {
+                return this.View();
+            }
+        }
+    }
+}
---
+++ PhotoLife/PhotoLife.Web/Area/Admin/Controllers/UserAdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhotoLife.Area.Admin.Controllers
+{
+    public class UserAdminController : Controller
+    {
+        // GET: UserAdmin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
