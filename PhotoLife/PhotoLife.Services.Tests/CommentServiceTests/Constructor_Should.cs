96948b7092a6a615e1210c7d0dba298e88f9f25f
Branimir <brani_svetoslavov@abv.bg>
2017-03-27 02:47:00 +0300

    Add comment service constructor tests

---
+++ PhotoLife/PhotoLife.Services.Tests/CommentServiceTests/Constructor_Should.cs
@@ -0,0 +1,41 @@
+﻿using Moq;
+using NUnit.Framework;
+using PhotoLife.Data.Contracts;
+using PhotoLife.Factories;
+using PhotoLife.Models;
+using PhotoLife.Providers.Contracts;
+using PhotoLife.Services.Contracts;
+
+namespace PhotoLife.Services.Tests.CommentServiceTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void _Initialize_NotNull_WhenEverythingPassedCorrectly()
+        {
+            //Arrange
+            var mockedPostService = new Mock<IPostService>();
+            var mockedNewsService = new Mock<INewsService>();
+            var mockedUserService = new Mock<IUserService>();
+            var mockedCommentFactory = new Mock<ICommentFactory>();
+            var mockedRepository = new Mock<IRepository<Comment>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedDateTimeProvider = new Mock<IDateTimeProvider>();
+
+            //Act
+            var commentService = new CommentService(
+                mockedPostService.Object,
+                mockedNewsService.Object,
+                mockedCommentFactory.Object,
+                mockedUserService.Object,
+                mockedRepository.Object,
+                mockedDateTimeProvider.Object,
+                mockedUnitOfWork.Object);
+
+            //Assert
+            Assert.IsNotNull(commentService);
+
+        }
+    }
+}
