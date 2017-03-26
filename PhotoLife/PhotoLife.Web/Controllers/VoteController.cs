fd75d5c6aac6e825c786061349011fcfc7e5c4c8
Branimir <brani_svetoslavov@abv.bg>
2017-03-27 01:36:21 +0300

    Add vote partial

---
+++ PhotoLife/PhotoLife.Web/Controllers/VoteController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PhotoLife.Authentication.Providers;
+using PhotoLife.Services.Contracts;
+
+namespace PhotoLife.Controllers
+{
+    public class VoteController : Controller
+    {
+        private readonly IVotingService voteService;
+        private readonly IAuthenticationProvider authenticationProvider;
+
+        public VoteController(IVotingService voteService, IAuthenticationProvider authenticationProvider)
+        {
+            if (voteService == null)
+            {
+                throw new ArgumentNullException(nameof(voteService));
+            }
+
+            if (authenticationProvider == null)
+            {
+                throw new ArgumentNullException(nameof(authenticationProvider));
+            }
+
+            this.voteService = voteService;
+            this.authenticationProvider = authenticationProvider;
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Vote(int logId, int currentVoteCount)
+        {
+            var currentUserId = this.authenticationProvider.CurrentUserId;
+
+            var rating = this.voteService.Vote(logId, currentUserId);
+
+            if (rating < 0)
+            {
+                rating = currentVoteCount;
+            }
+
+            return this.PartialView(rating);
+        }
+    }
+}
