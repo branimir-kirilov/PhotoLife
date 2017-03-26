850f50377356e6ae370ac21e9b7c093e54bc93b9
Branimir <brani_svetoslavov@abv.bg>
2017-03-27 01:19:22 +0300

    Add vote model

---
+++ PhotoLife/PhotoLife.Models/Vote.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PhotoLife.Models
+{
+    public class Vote
+    {
+        public Vote()
+        {
+
+        }
+
+        public Vote(int postId, string userId)
+        {
+            this.PostId = postId;
+            this.UserId = userId;
+        }
+
+        [Key]
+        public int VoteId { get; set; }
+
+        public string UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual User User { get; set; }
+
+        public int PostId { get; set; }
+
+        [ForeignKey("LogId")]
+        public virtual Post Post { get; set; }
+    }
+}
