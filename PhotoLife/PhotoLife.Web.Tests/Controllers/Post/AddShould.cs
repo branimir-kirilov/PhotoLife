a458c7b5aceab4c5eced11132d83005639fa0b7f
Branimir <brani_svetoslavov@abv.bg>
2017-03-27 04:04:50 +0300

    Add some post controller tests

---
+++ PhotoLife/PhotoLife.Web.Tests/Controllers/Post/AddShould.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using CloudinaryDotNet;
+using Moq;
+using NUnit.Framework;
+using PhotoLife.Authentication.Providers;
+using PhotoLife.Controllers;
+using PhotoLife.Factories;
+using PhotoLife.Services.Contracts;
+
+namespace PhotoLife.Web.Tests.Controllers.Post
+{
+    [TestFixture]
+    public class AddShould
+    {
+        [TestCase("fake", "fake", "fake")]
+        public void _Call_ViewModelFactory(string cloud, string apiKey, string apiSecret)
+        {
+            //Arrange
+            var mockedAuthProvider = new Mock<IAuthenticationProvider>();
+            var mockedPostService = new Mock<IPostService>();
+            var mockedViewModelFactory = new Mock<IViewModelFactory>();
+
+            var mockedAcc = new CloudinaryDotNet.Account(cloud, apiKey, apiSecret);
+            var mockedCloudinary = new Mock<Cloudinary>(mockedAcc);
+
+            var postController = new PostController(mockedAuthProvider.Object, mockedPostService.Object,
+                mockedViewModelFactory.Object, mockedCloudinary.Object);
+
+            //Act
+            var res = postController.Add() as ViewResult;
+
+            //Assert
+            mockedViewModelFactory.Verify(v => v.CreateAddPostViewModel(mockedCloudinary.Object), Times.Once);
+
+
+        }
+    }
+}
---
+++ PhotoLife/PhotoLife.Web.Tests/Controllers/Post/All_Should.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using CloudinaryDotNet;
+using Moq;
+using NUnit.Framework;
+using PhotoLife.Authentication.Providers;
+using PhotoLife.Controllers;
+using PhotoLife.Factories;
+using PhotoLife.Services.Contracts;
+
+namespace PhotoLife.Web.Tests.Controllers.Post
+{
+    public class All_Should
+    {
+        [TestCase("fake", "fake", "fake")]
+        public void _Call_PostService_GetAll(string cloud, string apiKey, string apiSecret)
+        {
+            //Arrange
+            var mockedAuthProvider = new Mock<IAuthenticationProvider>();
+            var mockedPostService = new Mock<IPostService>();
+            var mockedViewModelFactory = new Mock<IViewModelFactory>();
+
+            var mockedAcc = new CloudinaryDotNet.Account(cloud, apiKey, apiSecret);
+            var mockedCloudinary = new Mock<Cloudinary>(mockedAcc);
+
+            var postController = new PostController(mockedAuthProvider.Object, mockedPostService.Object,
+                mockedViewModelFactory.Object, mockedCloudinary.Object);
+
+            //Act
+            postController.All();
+
+            //Assert
+            mockedPostService.Verify(s => s.GetAll(), Times.Once);
+        }
+
+        [TestCase("fake", "fake", "fake")]
+        public void _Return_PartialView(string cloud, string apiKey, string apiSecret)
+        {
+            //Arrange
+            var expectedPartialName = "_PagedPostListPartial";
+
+            var mockedAuthProvider = new Mock<IAuthenticationProvider>();
+            var mockedPostService = new Mock<IPostService>();
+            var mockedViewModelFactory = new Mock<IViewModelFactory>();
+
+            var mockedAcc = new CloudinaryDotNet.Account(cloud, apiKey, apiSecret);
+            var mockedCloudinary = new Mock<Cloudinary>(mockedAcc);
+
+            var postController = new PostController(mockedAuthProvider.Object, mockedPostService.Object,
+                mockedViewModelFactory.Object, mockedCloudinary.Object);
+
+            //Act
+            var res = postController.All() as PartialViewResult;
+
+            //Assert
+            Assert.AreEqual(expectedPartialName, res.ViewName);
+
+        }
+    }
+}
