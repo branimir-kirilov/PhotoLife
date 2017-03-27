314b7994a449d2925f827a2666ee6d5f8f814697
Branimir <brani_svetoslavov@abv.bg>
2017-03-27 03:53:48 +0300

    Finish post controller constructor tests

--- PhotoLife/PhotoLife.Web.Tests/Controllers/Post/ConstructorShould.cs
+++ PhotoLife/PhotoLife.Web.Tests/Controllers/Post/ConstructorShould.cs
@@ -24,14 +24,84 @@ namespace PhotoLife.Web.Tests.Controllers.Post
             var mockedPostService = new Mock<IPostService>();
             var mockedViewModelFactory = new Mock<IViewModelFactory>();
 
-            var mockedAcc = new CloudinaryDotNet.Account(cloud,apiKey, apiSecret);
+            var mockedAcc = new CloudinaryDotNet.Account(cloud, apiKey, apiSecret);
             var mockedCloudinary = new Mock<Cloudinary>(mockedAcc);
 
             //Act
-            var postController = new PostController(mockedAuthProvider.Object, mockedPostService.Object, mockedViewModelFactory.Object, mockedCloudinary.Object)
+            var postController = new PostController(mockedAuthProvider.Object, mockedPostService.Object,
+                mockedViewModelFactory.Object, mockedCloudinary.Object);
 
             //Assert
             Assert.IsNotNull(postController);
+        }
+
+        [TestCase("fake", "fake", "fake")]
+        public void _Throws_ArgumentNullException_When_AuthProvider_IsNull(string cloud, string apiKey, string apiSecret)
+        {
+            //Arrange
+            var mockedPostService = new Mock<IPostService>();
+            var mockedViewModelFactory = new Mock<IViewModelFactory>();
+
+            var mockedAcc = new CloudinaryDotNet.Account(cloud, apiKey, apiSecret);
+            var mockedCloudinary = new Mock<Cloudinary>(mockedAcc);
+
+            //Act and Assert
+            Assert.Throws<ArgumentNullException>(() => new PostController(null, mockedPostService.Object,
+                mockedViewModelFactory.Object, mockedCloudinary.Object));
+        }
+
+        [TestCase("fake", "fake", "fake")]
+        public void _Throws_ArgumentNullException_When_PostService_IsNull(string cloud, string apiKey, string apiSecret)
+        {
+            //Arrange
+            var mockedAuthProvider = new Mock<IAuthenticationProvider>();
+            var mockedViewModelFactory = new Mock<IViewModelFactory>();
+
+            var mockedAcc = new CloudinaryDotNet.Account(cloud, apiKey, apiSecret);
+            var mockedCloudinary = new Mock<Cloudinary>(mockedAcc);
+
+            //Act and Assert
+            Assert.Throws<ArgumentNullException>(() => new PostController(
+                mockedAuthProvider.Object, 
+                null,
+                mockedViewModelFactory.Object,
+                mockedCloudinary.Object));
+        }
+
+        [TestCase("fake", "fake", "fake")]
+        public void _Throws_ArgumentNullException_When_ViewModelFactory_IsNull(string cloud, string apiKey, string apiSecret)
+        {
+            //Arrange
+            var mockedAuthProvider = new Mock<IAuthenticationProvider>();
+            var mockedPostService = new Mock<IPostService>();
+
+            var mockedAcc = new CloudinaryDotNet.Account(cloud, apiKey, apiSecret);
+            var mockedCloudinary = new Mock<Cloudinary>(mockedAcc);
+
+            //Act and Assert
+            Assert.Throws<ArgumentNullException>(() => new PostController(
+                mockedAuthProvider.Object,
+                mockedPostService.Object,
+                null,
+                mockedCloudinary.Object));
+        }
+
+
+        [TestCase("fake", "fake", "fake")]
+        public void _Throws_ArgumentNullException_WhenCloudinary_IsNull(string cloud, string apiKey, string apiSecret)
+        {
+            //Arrange
+            var mockedAuthProvider = new Mock<IAuthenticationProvider>();
+            var mockedPostService = new Mock<IPostService>();
+            var mockedViewModelFactory = new Mock<IViewModelFactory>();
+
+            //Act and Assert
+            Assert.Throws<ArgumentNullException>(() => new PostController(
+                mockedAuthProvider.Object,
+                mockedPostService.Object,
+                mockedViewModelFactory.Object,
+                null
+                ));
         }
     }
 }
